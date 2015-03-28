04a4f03fbb44a9998965951a8c7ac34f3ee74e0d
MarkTovkach <marktovkach@gmail.com>
2015-03-29 02:57:25 +0300

    7

---
+++ BookUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookLibrary;
+
+namespace BookUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book() { Author = "Алан Гинзберг", Title = "Вопль", ISBN = 226611156, Gerne = "Поэма" };
+            Book book1 = new Book() { Author = "Алан Гинзберг", Title = "Вопль", ISBN = 226611156, Gerne = "Поэма" };
+            Console.WriteLine(book.Equals(book1));
+
+            BookListService bookService = new BookListService();
+
+            //bookService.AddBook(new Book() { Author = "Алан Гинзберг", Title = "Вопль", ISBN = 226611156, Gerne = "Поэма" });
+            //bookService.AddBook(new Book() { Author = "Джек Керуак", Title = "В дороге", ISBN = 226611157, Gerne = "Роман" });
+
+            var bookList = bookService.GetAllBooks();
+            Console.WriteLine(bookList.ToArray()[0]);
+            Console.WriteLine(bookList.ToArray()[1]);
+         
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
